af72a21ea35bd95e10248aab63f23dd58ca5981e
tomo <1ornoln@gmail.com>
2026-04-03 17:47:57 +0900

    initial commit

--- src/main/main.js
+++ src/main/main.js
@@ -1,29 +1,27 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-function resolveEntryFile() {
-  const pageMap = {
-    timestretch:  '../../testsrc/timestretch_test/index.html',
-    audio_point:  '../../testsrc/audiosync_test/index.html',
-    pitchdrop:    '../../testsrc/pitchdrop_test/index.html',
-    granular:     '../../testsrc/granular_test/index.html',
-    granular_plus: '../../testsrc/granular_plus_test/index.html'
-  };
-
-  return pageMap[process.env.OTOMORA_TEST] || pageMap.granular_plus;
-}
+const pageMap = {
+  timestretch:  { file: '../../testsrc/timestretch_test/index.html',   width: 500, height: 260 },
+  audio_point:  { file: '../../testsrc/audiosync_test/index.html',     width: 500, height: 400 },
+  pitchdrop:    { file: '../../testsrc/pitchdrop_test/index.html',     width: 500, height: 260 },
+  granular:     { file: '../../testsrc/granular_test/index.html',      width: 500, height: 260 },
+  granular_plus: { file: '../../testsrc/granular_plus_test/index.html', width: 500, height: 260 },
+  visualizer:   { file: '../../testsrc/visualizer_test/index.html',    width: 900, height: 680 },
+};
 
 function createWindow() {
+  const cfg = pageMap[process.env.OTOMORA_TEST] || pageMap.granular_plus;
   const win = new BrowserWindow({
-    width: 500,
-    height: 260,
+    width: cfg.width,
+    height: cfg.height,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
     }
   });
 
-  win.loadFile(path.join(__dirname, resolveEntryFile()));
+  win.loadFile(path.join(__dirname, cfg.file));
 }
 
 app.whenReady().then(createWindow);
---
+++ testsrc/visualizer_test/v_claude.md
@@ -0,0 +1,18 @@
+electronを使用している。
+
+testsrc/visualizer_test内でのテスト。
+
+audioファイル
+assets/audio/001.wav
+
+jsonファイル
+assets/json/001.wav
+
+オーディオファイルを再生して
+
+jsonファイルに、モーラや単語ごとのパラメータが複数あるので、
+それらのパラメータを使って、視覚効果を作ってください。
+
+Generative Artみたいな、アート的な感じが良いです。
+
+これの実行コマンドも作ってください。
